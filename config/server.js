fa0e37bb73d27a40007ac77dd8c9373fc5ff63b9
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-02-07 18:22:09 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de86b437547551a47afca7cf5f81fc19'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '054042cd-4c9d-4323-ab90-bae984a40288'
+};
