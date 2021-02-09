2b3caaf4f223dc59b522c2902f6fe976f27b3da0
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-02-09 18:40:18 -0300

    Added getGameInfo function

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,4 +5,38 @@
  * to customize this service
  */
 
-module.exports = {};
+const axios = require('axios');
+
+const getGameInfo = async (slug) => {
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+
+  const body = await axios.get(`https://gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const ratingElement = dom.window.document.querySelector('.age-restrictions__icon use');
+
+  const description = dom.window.document.querySelector('.description');
+
+  return {
+    rating: ratingElement
+      ? ratingElement
+        .getAttribute('xlink:href')
+        .replace(/_/g, '')
+        .replace(/[^\w-]+/g, '')
+      : 'Livre',
+
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML,
+  }
+};
+
+module.exports = {
+  populate: async () => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const { data: { products } } = await axios.get(gogApiUrl);
+
+    return console.log(await getGameInfo(products[18].slug));
+  }
+};
